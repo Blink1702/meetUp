fcf99659695583d48a4c8604807f26501ec16739
Blink1702 <bernardo.oyervides17@gmail.com>
2024-05-08 15:57:16 -0500

    Initial commit

---
+++ src/test/java/edu/lawrence/meetUp/MeetUpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lawrence.meetUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
