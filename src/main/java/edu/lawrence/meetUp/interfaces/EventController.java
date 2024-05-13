e9952e79f9bd8e01ee6c802bf13c5aa7ef4eaf08
Blink1702 <bernardo.oyervides17@gmail.com>
2024-05-13 15:49:14 -0500

    finished Event controller

--- src/main/java/edu/lawrence/meetUp/interfaces/EventController.java
+++ src/main/java/edu/lawrence/meetUp/interfaces/EventController.java
@@ -1,18 +1,24 @@
 package edu.lawrence.meetUp.interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.lawrence.meetUp.entities.Event;
 import edu.lawrence.meetUp.interfaces.dtos.EventDTO;
 import edu.lawrence.meetUp.services.EventService;
+import edu.lawrence.meetUp.services.WrongUserException;
 
 @RestController
 @RequestMapping("/events")
@@ -26,10 +32,46 @@ public class EventController {
 	
 	@PostMapping
 	public ResponseEntity<String> save(@RequestBody EventDTO event){
-		String key = es.save(event);
+		String key;
 		
-		return ResponseEntity.ok().body(key);
+		try{
+			key = es.save(event);
+		} catch(WrongUserException ex) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invalid user id");
+		}
+			return ResponseEntity.ok().body(key);
 	}
+	
+	@GetMapping(params = {"place"})
+	public ResponseEntity<List<EventDTO>> finEventByPlace(@RequestParam(value = "place")String place){
+		List<Event> event = es.findEventByPlace(place);
+		List<EventDTO> results = new ArrayList<EventDTO>();
+		for(Event e : event) {
+			results.add(new EventDTO(e));
+		}
+		return ResponseEntity.ok().body(results);
+	}
+	
+	@GetMapping(params = {"time"})
+	public ResponseEntity<List<EventDTO>> finEventByTime(@RequestParam(value = "time")String time){
+		List<Event> event = es.findEventByTime(time);
+		List<EventDTO> results = new ArrayList<EventDTO>();
+		for(Event e : event) {
+			results.add(new EventDTO(e));
+		}
+		return ResponseEntity.ok().body(results);
+	}
+	
+	@GetMapping(params = {"sport"})
+	public ResponseEntity<List<EventDTO>> finEventBySport(@RequestParam(value = "sport")String sport){
+		List<Event> event = es.findEventBySport(sport);
+		List<EventDTO> results = new ArrayList<EventDTO>();
+		for(Event e : event) {
+			results.add(new EventDTO(e));
+		}
+		return ResponseEntity.ok().body(results);
+	}
+	
 	
 
 }
